5b82ae6dfaa1588c754b1c52b6694081a5b76305
Sajalhowlader <sajolhowlader08@gmail.com>
2022-06-09 16:25:00 +0600

    add find me

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaFacebook, FaGithub, FaLinkedin } from 'react-icons/fa';
 import bannerImg from '../../images/ppp.png';
 const Banner = () => {
     return (
@@ -8,6 +9,19 @@ const Banner = () => {
                   <p>WELCOME TO MY WORLD</p>
                 <h2>Hi ! I'm</h2>
                    <h1>SAJAL HOWLADER</h1>  
+                   <div>
+                     <p>FIND ME ON</p>
+                     <button className='social-btn'>
+                     <FaFacebook/>
+                     </button>
+                     <button className='social-btn'>
+                       <FaGithub/>
+                     </button>
+                     <button className='social-btn'>
+                       <FaLinkedin/>
+                     </button>
+                     
+                   </div>
               </div>
               <div className="banner-img">
                 <div className="img-shadow">
