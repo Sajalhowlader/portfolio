cc14d975ff64dbb034a44f5f6cb9a076eac21c97
Sajalhowlader <sajolhowlader08@gmail.com>
2022-06-09 14:13:11 +0600

    add a pic

--- src/Pages/Home/Header.js
+++ src/Pages/Home/Header.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import navImg from '../../images/oo.jpg'
 
 const Header = () => {
   return (
     <>
       <Navbar collapseOnSelect expand="lg"  className='px-5 nav-con'>
-   <Navbar.Brand href="#home">SAJAL</Navbar.Brand>
+   <Navbar.Brand href="#home" className='navImg'><img  src={navImg} alt="" /> <span>SAJAL</span></Navbar.Brand>
    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
    <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="ms-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <NavLink className='home-menu' to="/">HOME</NavLink>
+      <NavLink className='home-menu' to="/">FEATURES</NavLink>
+      <NavLink className='home-menu' to="/">PROJECTS</NavLink>
+      <NavLink className='home-menu' to="/">CONTACTS</NavLink>
     </Nav>
     <Nav>
-      <Nav.Link eventKey={2} href="#memes">
+      <NavLink  to="/">
        <button className='resume-btn'>Download Resume</button>
-      </Nav.Link>
+      </NavLink>
     </Nav>
    </Navbar.Collapse>
 </Navbar>
