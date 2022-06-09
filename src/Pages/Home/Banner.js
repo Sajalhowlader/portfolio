5c6b39dda336a83c5eec6e5434fc8bf9192bea47
Sajalhowlader <sajolhowlader08@gmail.com>
2022-06-09 14:36:58 +0600

    add red

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import bannerImg from '../../images/linked profile.jpg'
+import bannerImg from '../../images/pp.png';
 const Banner = () => {
     return (
         <section>
           <div className='banner-container'>
               <div className='banner-info'>
+ <h2>Hi ! I'm</h2>
+ <h1>SAJAL HOWLADER</h1>
 
               </div>
               <div className="banner-img">
